829a5cc1b44c618a4db929162282766e81a31bbe
Daniil <dtuneev@gmail.com>
2023-11-04 15:50:44 +0200

    changing root file + adding tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,33 +1,67 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { LiveReload, Link, Links, Meta, MetaFunction, Outlet } from "@remix-run/react";
+import * as process from "process";
+import stylesheet from "~/styles/tailwind.css";
 
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+export const links = () => [
+    { rel: "stylesheet", href: stylesheet },
 ];
 
+export const meta: MetaFunction = () => {
+    const title = "Remix blog";
+    const description = "A blog built with Remix";
+    const keywords = "remix, react, javascript, typescript, css, html";
+
+    return [
+        { name: "keywords", content: keywords },
+        { name: "title", content: title },
+        { name: "description", content: description },
+    ];
+}
+
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+    return (
+        <Document>
+            <Layout>
+                <Outlet />
+            </Layout>
+        </Document>
+    );
+}
+
+function Document({ children, title='Remix blog' }: { children: React.ReactNode; title?: string; }) {
+    return (
+        <html lang='en'>
+            <head>
+                <meta charSet='utf-8'/>
+                <meta name='viewport' content='width=device-width, initial-scale=1'/>
+                <Links />
+                <Meta />
+            </head>
+            <body>
+                {children}
+                { process.env.NODE_ENV === "development" && <LiveReload /> }
+            </body>
+        </html>
+    );
+
+}
+
+function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <nav className="navbar">
+                <Link to='/' className='logo'>Blog</Link>
+
+                <ul className="nav">
+                    <li>
+                        <Link to='/posts'>Posts</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <div className="container">
+                {children}
+            </div>
+        </>
+    )
 }
